0dd3b97fd176193e72e2a8c1e465e4319d924d50
Havilash <92213955+Havilash@users.noreply.github.com>
2022-06-06 20:56:33 +0200

    finish AI

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-import pygame
-import random
 import os
+import sys
+import pygame
 import neat
-
+import random
+import pickle
+import re
 
 WIDTH = 500
 HEIGHT = 500
 ROWS = 20
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+FPS = 1000
+gen = 0
 
 
 class Cube():
@@ -44,43 +48,30 @@ class Cube():
 
 class Snake():
     rows = ROWS
-    body = []
-    turns = {}
-    is_dying = False
+    # body = []
 
     def __init__(self, color, pos):
+        self.apple_frame_count = 0
+
         self.color = color
+        self.body = []
         self.head = Body(pos)  # set head to front
         self.body.append(self.head)  # add cube to body
 
-        self.dirnx = 0  # Direction x, y
-        self.dirny = 1
+        self.is_dying = False
+        self.turns = {}
+
+        self.dirnx = 0  # Direction x
+        self.dirny = 1  # Direction y
+
+
+        self.apple = Apple(randomSnack(ROWS, self))
+
 
     def move(self):
-        # for event in pygame.event.get():
-        #     if event.type == pygame.QUIT:  # check if user wants to close window
-        #         pygame.quit()
 
-        #     keys = pygame.key.get_pressed()  # get pressed keys
 
-        #     for key in keys:  # loop through all keys
-        #         if keys[pygame.K_LEFT]:
-        #             self.dirnx = -1
-        #             self.dirny = 0
-                    
-        #         elif keys[pygame.K_RIGHT]:
-        #             self.dirnx = 1
-        #             self.dirny = 0
-
-        #         elif keys[pygame.K_UP]:
-        #             self.dirnx = 0
-        #             self.dirny = -1
-
-        #         elif keys[pygame.K_DOWN]:
-        #             self.dirnx = 0
-        #             self.dirny = 1
-
-        #     self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+        self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
 
 
         for i, b in enumerate(self.body):  # loop through all cubes in body
@@ -96,7 +87,6 @@ class Snake():
                 # check if reached the edge
                 if b.dirnx == -1 and b.pos[0] <= 0 or b.dirnx == 1 and b.pos[0] >= b.rows - 1 or b.dirny == 1 and b.pos[
                     1] >= b.rows - 1 or b.dirny == -1 and b.pos[1] <= 0:
-                    self.die()
                     pass
                 else:
                     b.move(b.dirnx, b.dirny)
@@ -129,6 +119,13 @@ class Snake():
         self.body[-1].dirnx = dx
         self.body[-1].dirny = dy
 
+    def spawn_apple(self):
+        if self.body[0].pos == self.apple.pos:  # when apple is eaten
+            self.addCube()
+            self.apple = Apple(randomSnack(ROWS, self))
+            self.apple_frame_count = 0
+            return True
+
     def draw(self, surface, eyes=False):
         for i, c in enumerate(self.body):
             if i == 0:  # for the first cube in the list we want to draw eyes
@@ -137,19 +134,33 @@ class Snake():
                 c.draw(surface)  # otherwise we will just draw a cube
 
     def die(self):
-        print('Score: ', len(self.body))
-        self.reset((10, 10))
+        # print('Score: ', len(self.body))
+        # self.reset((10, 10))
         self.is_dying = True
 
     def get_data(self):
-        x_dist_wall = self.rows - self.head.pos[0]
-        y_dist_wall = self.rows - self.head.pos[1]
-        x_dist_tail = self.body[-1].pos[0] - self.head.pos[0]
-        y_dist_tail = self.body[-1].pos[1] - self.head.pos[1]
-        # x_dist_food = 0
-        # y_dist_food = 0
+        # Distance to the Walls
+        # dist_wall_right = self.rows - self.head.pos[0]
+        # dist_wall_bottom = self.rows - self.head.pos[1]
+        # dist_wall_left = self.head.pos[0]
+        # dist_wall_top = self.head.pos[1]
 
-        return (x_dist_wall, y_dist_wall, x_dist_tail, y_dist_tail)
+        # x_dist_tail = self.body[-1].pos[0] - self.head.pos[0]
+        # y_dist_tail = self.body[-1].pos[1] - self.head.pos[1]
+
+        x_dist_food = self.apple.pos[0] - self.head.pos[0]
+        y_dist_food = self.apple.pos[1] - self.head.pos[1]
+
+        is_obstacle = [[],[],[],[]]
+        for i, pos_val in enumerate([(0, 1), (0, -1), (1, 0), (-1, 0)]):
+            pos = (self.head.pos[0] + pos_val[0], self.head.pos[1] + pos_val[1])
+            if pos in list(map(lambda z: z.pos, self.body[1:])) or (pos[0] < 0 and pos[0] >= ROWS and pos[1] >= ROWS and pos[1] < 0):  # check if body overlap
+                is_obstacle[i] = 1
+                continue
+            is_obstacle[i] = 0
+
+        return (x_dist_food, y_dist_food, *is_obstacle)
+        # return (x_dist_food, y_dist_food)
 
 
 class Body(Cube):
@@ -180,12 +191,12 @@ def drawGrid(w, rows, win):
         pygame.draw.line(win, (255, 255, 255), (0, y), (w, y))
 
 
-def redrawWindow(win, snakes, apple):
+def redrawWindow(win, snakes):
     win.fill((0, 0, 0))
     
     for snake in snakes:
         snake.draw(win)
-    apple.draw(win)
+        snake.apple.draw(win)
     drawGrid(WIDTH, ROWS, win)
     pygame.display.update()
 
@@ -204,7 +215,10 @@ def randomSnack(rows, item):
     return (x, y)
 
 
-def main(genomes, config):
+def eval_genomes(genomes, config):
+    global gen, FPS
+    APPLE_TIME = 100  # livetime without eating apples
+
     nets = []
     ge = []
     snakes = []
@@ -216,33 +230,26 @@ def main(genomes, config):
         snakes.append(Snake((255, 0, 0), (10, 10)))
         ge.append(genome)
 
-    # snakes.pop(0)
-    # ge.pop(0)
-    # nets.pop(0)
-
-    apple = Apple(randomSnack(ROWS, snakes[0]))
-
+    gen += 1
 
     is_running = True
 
     clock = pygame.time.Clock()
+    
     while is_running and len(snakes) > 0:
-        clock.tick(5)  # Set frames per second to 10
+        clock.tick(FPS)  # Set frames per second to 10
         for event in pygame.event.get():  # Quits Pygame
             if event.type == pygame.QUIT:
                 is_running = False
                 pygame.quit()
-                break
-
+                sys.exit()
 
         # Evalulate Data
         for x, snake in enumerate(snakes):
+            snake.apple_frame_count += 1
             ge[x].fitness += 0.1
 
-            x_dist_food = apple.pos[0] - snake.head.pos[0]
-            y_dist_food = apple.pos[1] - snake.head.pos[1]
             data = snake.get_data()
-            data = (*data, x_dist_food, y_dist_food)
 
             output = nets[x].activate(data)  # Neural Network Output
 
@@ -262,48 +269,84 @@ def main(genomes, config):
                 snake.dirnx = 0
                 snake.dirny = 1
                 
-            snake.turns[snake.head.pos[:]] = [snake.dirnx, snake.dirny]
-
-        for snake in snakes:  
+        for i, snake in enumerate(snakes):  
             snake.move()
-            if snake.body[0].pos == apple.pos:  # when apple is eaten
-                snake.addCube()
-                apple = Apple(randomSnack(ROWS, snake))
+            if snake.spawn_apple():
+                ge[i].fitness += 5
+            
 
         for i, snake in enumerate(snakes):
-            for x in range(len(snake.body)-1):
-                if snake.body[x].pos in list(map(lambda z: z.pos, snake.body[x + 1:])):
-                    # This will check if any of the positions in our body list overlap
-                    # print("die")
-                    snake.die()
-                    # break
+            if snake.head.pos in list(map(lambda z: z.pos, snake.body[1:])):  # check if body overlap
+                # print("overlap death")
+                snake.die()
+                break
+
+            if snake.dirnx == -1 and snake.head.pos[0] < 0 or snake.dirnx == 1 and snake.head.pos[0] >= ROWS or snake.dirny == 1 and snake.head.pos[1] >= ROWS or snake.dirny == -1 and snake.head.pos[1] < 0:
+                # print("edge death")
+                snake.die()
+
+            if snake.apple_frame_count > APPLE_TIME:
+                # print("apple death")
+                ge[i].fitness -= 7
+                snake.die()
             
+        for i, snake in enumerate(snakes):
             if snake.is_dying:
-                ge[i].fitness -= 1.5
+                ge[i].fitness -= 2
                 snakes.pop(i)
                 ge.pop(i)
                 nets.pop(i)
 
+        # print(list(map(lambda x: x.fitness, ge)), list(map(lambda x: x.apple_frame_count, snakes)))
 
-        redrawWindow(WIN, snakes, apple)  # Refresh screen
-    pygame.time.delay(3000)
+        redrawWindow(WIN, snakes)  # Refresh screen
 
-def run(config_file):
+def run(config_path):
+    global FPS
+
     config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction,
                          neat.DefaultSpeciesSet, neat.DefaultStagnation,
-                         config_file)
+                         config_path)
 
-    p = neat.Population(config)
+
+    for root, dirs, files in os.walk('checkpoints'):
+        checkpoint_nums = list(map(lambda x: int(re.search('[0-9]*$', x).group(0)), files))
+        best_checkpoint = max(checkpoint_nums)
+            
+    p = neat.Checkpointer.restore_checkpoint('checkpoints/neat-checkpoint-' + str(best_checkpoint))
 
     p.add_reporter(neat.StdOutReporter(True))
     stats = neat.StatisticsReporter()
     p.add_reporter(stats)
+    p.add_reporter(neat.Checkpointer(50, filename_prefix='checkpoints/neat-checkpoint-'))
 
-    winner = p.run(main, 100)
+    # pe = neat.ThreadedEvaluator(1 + multiprocessing.cpu_count(), eval_genomes)
+    # winner = p.run(pe.evaluate, 10)
+
+    winner = p.run(eval_genomes, 300)
+
+    print("[SAVING] saving best genome ...")
+    pickle.dump(winner, open('save_winner.pickle', 'wb'))
 
     print('\nBest genome:\n{!s}'.format(winner))
+
+    FPS = 20
+    replay_genome(config_path, "save_winner.pickle")
+
+
+
+def replay_genome(config_path, genome_path):
+    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                         config_path)
+
+    genome = pickle.load(open(genome_path, "rb"))
+
+    eval_genomes([(1, genome)], config)
 
 if __name__ == '__main__':
     local_dir = os.path.dirname(__file__)
     config_path = os.path.join(local_dir, 'config-feedforward.txt')
+
     run(config_path)
+    # replay_genome(config_path, "save_genome.pickle")
